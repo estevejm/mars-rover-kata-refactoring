07486075d664f17b54bda4e96de25649949b0493
Esteve Julià <ejulimelis@ebay.com>
2016-04-19 22:53:22 +0200

    removed unused methods

--- src/Point.java
+++ src/Point.java
@@ -24,22 +24,6 @@ public class Point {
         this.y += 1;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
